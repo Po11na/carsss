f504aafdc63394fae7cfcb8c49d208c60bb39198
Polina <po11nakchik@gmail.com>
2022-12-24 00:44:06 +0400

    Создание формы авторизации

--- carsss/Auth.Designer.cs
+++ carsss/Auth.Designer.cs
@@ -29,32 +29,153 @@ namespace carsss
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Auth));
             this.button1 = new System.Windows.Forms.Button();
+            this.loginField = new System.Windows.Forms.TextBox();
+            this.passField = new System.Windows.Forms.TextBox();
+            this.Логин = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.MainPlane = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPlane)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(329, 252);
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(226, 319);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(271, 43);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Войти";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // loginField
+            // 
+            this.loginField.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.loginField.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.loginField.Location = new System.Drawing.Point(226, 147);
+            this.loginField.Multiline = true;
+            this.loginField.Name = "loginField";
+            this.loginField.Size = new System.Drawing.Size(271, 44);
+            this.loginField.TabIndex = 2;
+            // 
+            // passField
+            // 
+            this.passField.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.passField.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.passField.Location = new System.Drawing.Point(226, 248);
+            this.passField.Name = "passField";
+            this.passField.Size = new System.Drawing.Size(271, 31);
+            this.passField.TabIndex = 3;
+            this.passField.UseSystemPasswordChar = true;
+            // 
+            // Логин
+            // 
+            this.Логин.AutoSize = true;
+            this.Логин.Font = new System.Drawing.Font("Montserrat", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Логин.Location = new System.Drawing.Point(233, 109);
+            this.Логин.Name = "Логин";
+            this.Логин.Size = new System.Drawing.Size(68, 24);
+            this.Логин.TabIndex = 4;
+            this.Логин.Text = "Логин";
+            this.Логин.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 10F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(233, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Пароль";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.AutoSize = true;
+            this.closeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeButton.Font = new System.Drawing.Font("Montserrat", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.closeButton.Location = new System.Drawing.Point(678, 0);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(37, 39);
+            this.closeButton.TabIndex = 6;
+            this.closeButton.Text = "X";
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox1.Image = global::carsss.Properties.Resources.Vector_1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(52, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainPlane
+            // 
+            this.MainPlane.Image = ((System.Drawing.Image)(resources.GetObject("MainPlane.Image")));
+            this.MainPlane.Location = new System.Drawing.Point(-2, 0);
+            this.MainPlane.Name = "MainPlane";
+            this.MainPlane.Size = new System.Drawing.Size(717, 499);
+            this.MainPlane.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.MainPlane.TabIndex = 10;
+            this.MainPlane.TabStop = false;
+           
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(253, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(227, 38);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Авторизация";
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
             // 
             // Auth
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(715, 498);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Логин);
+            this.Controls.Add(this.passField);
+            this.Controls.Add(this.loginField);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.MainPlane);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Auth";
             this.Text = "Auth";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPlane)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox loginField;
+        private System.Windows.Forms.TextBox passField;
+        private System.Windows.Forms.Label Логин;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label closeButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox MainPlane;
+        private System.Windows.Forms.Label label3;
     }
 }
--- carsss/Auth.cs
+++ carsss/Auth.cs
@@ -15,6 +15,48 @@ namespace carsss
         public Auth()
         {
             InitializeComponent();
+
+            this.passField.AutoSize = false;
+            this.passField.Size = new Size(this.passField.Width, 44);
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        Point lastPoint;
+
+
+        //private void MainPlane_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    if(e.Button = MouseButtons.Left)
+        //    {
+        //        this.Left
+        //    }
+
+        //}
     }
 }
